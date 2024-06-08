8a47ceac152e8540a54ce260a426e47ad4a654c1
vishal kumar <vishalkumar36565@gmail.com>
2024-06-08 10:29:08 +0530

    bug fixed

--- weather.js
+++ weather.js
@@ -4,6 +4,7 @@ window.addEventListener("load", () => {
     navigator.geolocation.getCurrentPosition((position) => {
       let lon = position.coords.longitude;
       let lat = position.coords.latitude;
+
       const url =
         `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&` +
         `lon=${lon}&appid=${apikey}`;
@@ -15,8 +16,12 @@ window.addEventListener("load", () => {
         .then((data) => {
           console.log(data);
           console.log(new Date().getTime());
-          var dat = new Date(data.dt);
-          console.log(dat.toLocaleString(undefined, "Asia/Kolkata"));
+
+          var dat = new Date(data.dt * 1000);
+
+          console.log(
+            dat.toLocaleString("en-US", { timeZone: "Asia/Kolkata" })
+          );
           console.log(new Date().getMinutes());
           weatherReport(data);
         });
